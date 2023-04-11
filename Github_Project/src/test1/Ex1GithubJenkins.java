c8dcc440a717dc5825dad607a002530eeab2fa63
trupt <trupt@192.168.0.19>
2023-04-11 17:21:12 -0400

    Last change done.

---
+++ Github_Project/src/test1/Ex1GithubJenkins.java
@@ -0,0 +1,11 @@
+package test1;
+
+public class Ex1GithubJenkins {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello There !!");
+
+	}
+
+}
